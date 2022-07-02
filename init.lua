a7c785d70c8b5ce269594c531d707da597a4b32c
Mir Sole <zburn101@gmail.com>
2022-07-02 21:28:25 +1000

    mould

--- init.lua
+++ init.lua
@@ -1,43 +1,38 @@
-if ModIsEnabled( "mould_n" ) then
-	ModMagicNumbersFileAdd( "mods/mould_asmallworld/files/magic_numbers.xml" ) 
+ModMagicNumbersFileAdd( "mods/mould_theotherside/files/magic_numbers.xml" ) 
 
-	local nxml = dofile_once("mods/mould_n/lib/nxml.lua")
+local nxml = dofile_once("mods/mould_theotherside/lib/nxml.lua")
 	
-	local content = ModTextFileGetContent("data/biome/_biomes_all.xml")
-	local xml = nxml.parse(content)
-	xml:add_children(nxml.parse_many[[
+local content = ModTextFileGetContent("data/biome/_biomes_all.xml")
+local xml = nxml.parse(content)
+xml:add_children(nxml.parse_many[[
 		
-	]])
-	ModTextFileSetContent("data/biome/_biomes_all.xml", tostring(xml))
-	
-	
-	
-	function OnModInit()
-		print("Mod - OnModInit()")
-	end
-	
-	function OnModPostInit()
-		print("Mod - OnModPostInit()")
-	end
-	
-	function OnPlayerSpawned( player_entity )
-	
-	end
-	
-	function OnWorldPostUpdate()
-	
-	end
-	
-	function OnPlayerDied()
-		print("dead")
-	end
-	
-	function Moulding()
-	
-	end
-	
-	GamePrint("A Small World")
-else
-	GamePrint("Mould is required for A Small World!")
-end
+]])
+ModTextFileSetContent("data/biome/_biomes_all.xml", tostring(xml))
 
+
+	
+function OnModInit()
+	print("Mod - OnModInit()")
+end
+	
+function OnModPostInit()
+	print("Mod - OnModPostInit()")
+end
+	
+function OnPlayerSpawned( player_entity )
+
+end
+	
+function OnWorldPostUpdate()
+
+end
+	
+function OnPlayerDied()
+	print("dead")
+end
+	
+function Moulding()
+
+end
+	
+GamePrint("A Small World")
